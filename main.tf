d73af4ab9490188379c73babb412c173263c2720
Marvin Curlee <marvin.curlee@gmail.com>
2025-07-08 12:20:56 -0500

    1

--- main.tf
+++ main.tf
@@ -30,12 +30,4 @@ resource "aws_instance" "example2" {
   }
 }
 
-resource "aws_instance" "example3" {
-  ami           = "ami-054d057aaa6f1aa39"  # Amazon Linux 2 AMI for us-east-1 (update for other regions)
-  instance_type = "t2.micro"
-  subnet_id     = "subnet-06e8cc9ff8751a5a1"
 
-  tags = {
-    name = "example-2}"
-  }
-}
