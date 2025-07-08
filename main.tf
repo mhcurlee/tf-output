db6ff148ceb65fafa7eec34c13e4b5cfd6ba7afe
Marvin Curlee <marvin.curlee@gmail.com>
2025-07-08 12:31:30 -0500

    1

--- main.tf
+++ main.tf
@@ -6,28 +6,29 @@ provider "aws" {
 }
 
 
-output "TESTME" {
+ output "TESTME" {
     value = "TEST 1234"
 }
 
-resource "aws_instance" "example1" {
-  ami           = "ami-054d057aaa6f1aa39"  # Amazon Linux 2 AMI for us-east-1 (update for other regions)
-  instance_type = "t2.micro"
-  subnet_id     = "subnet-06e8cc9ff8751a5a1"
 
-  tags = {
-    name = "example-1}"
-  }
-}
+#resource "aws_instance" "example1" {
+#  ami           = "ami-054d057aaa6f1aa39"  # Amazon Linux 2 AMI for us-east-1 (update for other regions)
+#  instance_type = "t2.micro"
+#  subnet_id     = "subnet-06e8cc9ff8751a5a1"
 
-resource "aws_instance" "example2" {
-  ami           = "ami-054d057aaa6f1aa39"  # Amazon Linux 2 AMI for us-east-1 (update for other regions)
-  instance_type = "t2.micro"
-  subnet_id     = "subnet-06e8cc9ff8751a5a1"
+#  tags = {
+#    name = "example-1}"
+#  }
+#}
 
-  tags = {
-    name = "example-2}"
-  }
-}
+#resource "aws_instance" "example2" {
+#  ami           = "ami-054d057aaa6f1aa39"  # Amazon Linux 2 AMI for us-east-1 (update for other regions)
+#  instance_type = "t2.micro"
+#  subnet_id     = "subnet-06e8cc9ff8751a5a1"
+
+#  tags = {
+#    name = "example-2}"
+#  }
+#}
 
 
